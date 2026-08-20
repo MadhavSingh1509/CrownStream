39e9669dbabe26c07ca426098c07c1abf74a3468
Madhav Singh <adityamadhav1509@gmail.com>
2026-08-20 11:03:54 +0530

    updating routes

--- src/app.js
+++ src/app.js
@@ -11,5 +11,16 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
+
+
+//importing routes
+import userRouter from './routes/user.routes.js'
+
+
+//app.get nahi kaam krega beta
+//route declaration:
+app.use("/api/v1/users",userRouter)
+
+//http://localhost:8000/api/v1/users/register
 export {app}
 
